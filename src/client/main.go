c07d70cfa4b30fbb7a9ed04737911b2283381b80
Koala Kang <kangwei19@live.com>
2020-05-26 09:46:41 +0800

    Initial commit

---
+++ src/client/main.go
@@ -0,0 +1,41 @@
+/*
+@Time : 2020/5/25 15:32
+@Author : wkang
+@File : client
+@Description:
+*/
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	pb "../helloworld"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("####### get server Greeting response: %s", r.Message)
+}
---
+++ src/server/main.go
@@ -0,0 +1,44 @@
+/*
+@Time : 2020/5/25 15:01
+@Author : wkang
+@File : server
+@Description:
+*/
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	pb "../helloworld"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	fmt.Println("######### get client request name :"+in.Name)
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
